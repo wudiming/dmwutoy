5f3b6222ee4913f467c32d8b93c2f4d9636826c0
wudiming <sxwudiming@163.com>
2026-04-14 04:12:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ cd /app
 
 # 确保必要的目录存在
 mkdir -p /app/iso /app/data /app/log
+
+# 【新增关键步骤】：如果映射的 data 目录是空的，就把模板文件复制进去
+if [ ! -f "/app/data/iventoy.dat" ]; then
+    echo "初始化 data 目录..."
+    cp -a /app/data_template/* /app/data/
+fi
+
 touch /app/log/log.txt
 
 # 启动 iVentoy
